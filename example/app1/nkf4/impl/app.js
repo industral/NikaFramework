703491f54a3f1934675d697565ae30570d15bcb1
Alex Ivasyuv <alex@siegerstein.com>
2013-01-13 00:48:22 +0200

    - fixes

--- example/app1/nkf4/impl/app.js
+++ example/app1/nkf4/impl/app.js
@@ -1,9 +1,5 @@
 $(function() {
-  console.log(1);
   nkf.conf.usePageLoadStandardBehaviour = false;
-
-  var url = "/" + nkf.core.Controller.getCurrentPath();
-  history.replaceState({path: url, init: true}, "", url);
 
   $(document).trigger("nkf.core.Controller", {
     type: nkf.def.events.type.make,
